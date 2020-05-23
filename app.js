a5d54fad0f3a4821c30b863cf0b7439cf8e488bf
BogdanM <shade_mbogdan@yahoo.com>
2020-05-23 20:09:40 +0300

    Set up board and mole graphic

---
+++ app.js
@@ -0,0 +1,5 @@
+const square = document.querySelectorAll('.tile');
+const mole = document.querySelectorAll('.mole');
+const timer = document.querySelector('#timer');
+let scoer = document.querySelector('#score');
+
